dcd94f526809874f556aa951d94444adab2a444f
Dwi Sam <dwisamsiarto@gmail.com>
2022-09-17 09:35:36 +0700

    fix

--- sesi-07/hello-mounting/src/components/Halu.js
+++ sesi-07/hello-mounting/src/components/Halu.js
@@ -1,29 +1,21 @@
-import React from "react";
+import React, { Component } from "react";
 
-class Halu extends React.Component {
+export default class Halu extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      count: 0,
-      users: [],
       isLoading: true,
       data: [],
     };
   }
 
-  shouldComponentUpdate(nextProps, nextState) {
-    if (this.state.users.length !== nextState.users.length) {
-      return true;
-    }
-
-    return false;
-  }
-
-  componentDidUpdate(preProps) {
+  componentDidUpdate(prevProps) {
     if (prevProps.selectedState !== this.props.selectedState) {
       fetch("https://pathToApi.com")
         .then((resp) => resp.json())
         .then((respJson) => {
+          // do what ever you want with your 'response'
           this.setState({
             isLoading: false,
             data: respJson,
@@ -35,13 +27,7 @@ class Halu extends React.Component {
     }
   }
 
-  componentDidMount() {
-    eventData.addEventListener();
-  }
-
-  componentWillUnmount() {
-    eventDAta.removeEventListener();
+  render() {
+    return <div>Halu</div>;
   }
 }
-
-export default Halu;
