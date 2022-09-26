55b98c1568a242742f285091ae368d27d789a45f
Dwi Sam <dwisamsiarto@gmail.com>
2022-09-26 22:32:57 +0700

    fix

--- sesi-09/hello-route/src/App.js
+++ sesi-09/hello-route/src/App.js
@@ -1,43 +1,44 @@
-import { Route, Routes } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/navbar";
 import ProtectedRoute from "./components/protectedRoute";
-import AboutCompany from "./pages/aboutCompany";
-import Home from "./pages/home";
-import Item from "./pages/item";
-import Login from "./pages/login";
-import Member from "./pages/member";
-import Members from "./pages/members";
-import About from "./pages/about";
-import AboutMe from "./pages/aboutMe";
-import Items from "./pages/items";
 
-function App() {
-	return (
-		<div className="container">
-			<Navbar />
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/about" element={<About />}>
-					<Route path="about-company" element={<AboutCompany />} />
-					<Route path="about-me" element={<AboutMe />} />
-				</Route>
-				<Route path="/members" element={<Members />}>
-					<Route path=":memberName" element={<Member />} />
-				</Route>
-				<Route 
-					path="/items"
-					element={
-						<ProtectedRoute>
-							<Items />
-						</ProtectedRoute>
-					}>
-						<Route path=":itemId" element={<Item />} />
-				</Route>
-				<Route path="/login" element={<Login />} />
-			</Routes>
-		</div>
-	);
+import Home from "./pages/home";
+import About from "./pages/about";
+import AboutCompany from "./pages/aboutCompany";
+import AboutMe from "./pages/aboutMe";
+import Members from "./pages/members";
+import Member from "./pages/member";
+import Login from "./pages/login";
+import Items from "./pages/items";
+import Item from "./pages/item";
+
+const App = () => {
+  return (
+    <div className="container">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />}>
+          <Route path="about-company" element={<AboutCompany />} />
+          <Route path="about-me" element={<AboutMe />} />
+        </Route>
+        <Route path="/members" element={<Members />}>
+          <Route path=":memberName" element={<Member />} />
+        </Route>
+        <Route
+          path="/items"
+          element={
+            <ProtectedRoute>
+              <Items />
+            </ProtectedRoute>
+          }>
+          <Route path=":itemId" element={<Item />} />
+        </Route>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- sesi-09/hello-route/src/pages/items.js
+++ sesi-09/hello-route/src/pages/items.js
@@ -1,31 +1,35 @@
-import React from "react";
 import { Outlet } from "react-router-dom";
 import { getItems } from "../items";
 import ItemRow from "../components/itemRow";
 
 const Items = () => {
-  const items = getItems();
+    const items = getItems();
 
-  return (
-    <div>
-      <h1>Items page</h1>
-      <p>Lorem ipsum dolor sit.</p>
-      <table>
-        <thead>
-          <tr>
-            <th>Item</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {items.map((item) => (
-            <ItemRow key={item.id} item={item} />
-          ))}
-        </tbody>
-      </table>
-      <Outlet />
-    </div>
-  );
-};
+    return (
+        <>
+            <h1>Items page</h1>
+            <p>Select an item to be shown</p>
+            <table>
+                <thead>
+                <tr>
+                    <th>Item</th>
+                    <th>Action</th>
+                </tr>
+                </thead>
+                <tbody>
+                {
+                    items.map(item => {
+                        return (
+                            <ItemRow key={item.id} item={item} />
+                        )
+                    })
+                }
+                </tbody>
+            </table>
+
+            <Outlet />
+        </>
+    )
+}
 
 export default Items;
