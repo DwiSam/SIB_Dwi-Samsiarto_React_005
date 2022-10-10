7b5b2f78082a33272b8a81fc5d73e57d62d938b0
Dwi Sam <dwisamsiarto@gmail.com>
2022-10-10 23:22:31 +0700

    add login-app

---
+++ Self-Paced-Learning/login-app/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import {Routes , Route} from "react-router-dom"
+import Home from './pages/Home';
+import UserDetail from "./components/UserDetail/UserDetail"
+import UsersList from "./pages/UsersList"
+import LoginForm from './components/LoginForm/LoginForm';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/users/:id' element={<UserDetail />} />
+        <Route path='/users-list' element={<UsersList />} />
+        <Route path='/login' element={<LoginForm />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
